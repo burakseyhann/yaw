815a67633e5a67738cefea10c3c27f823c15c29b
burakseyhann <burakseyhan99@hotmail.com>
2021-01-31 23:04:17 +0300

    login
    login

--- App.js
+++ App.js
@@ -1,13 +1,54 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {
+  Text,
+  View,
+  TextInput,
+  SafeAreaView,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import Ionicons from "react-native-vector-icons/Ionicons";
+import LinearGradient from 'react-native-linear-gradient';
 
-const App = () => (
-  <View>
-    <Text>Login Screenn</Text>
-    <Ionicons name={'ios-menu-outline'} color='red' size={35} />
-  </View>
-);
-
+function App() {
+  return (
+    //colors={['rgba(65,29,108)','rgba(198, 166, 236)']}
+    <SafeAreaView style={{flex: 1}}>
+      <LinearGradient  colors={['#4c669f', '#3b5998', '#192f6a']} >
+        <View style={styles.headerContainer}>
+          <View>
+            <Text>lsakdljsajkda</Text>
+          </View>
+        </View>
+      </LinearGradient>
+      <View style={styles.mainContainer}>
+        <TextInput />
+        <TextInput />
+        <TouchableOpacity></TouchableOpacity>
+        <Icon />
+        <TouchableOpacity></TouchableOpacity>
+        <TouchableOpacity></TouchableOpacity>
+      </View>
+      <View style={styles.footerContainer}>
+        <TouchableOpacity>
+          <Text>asdfadad</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+}
 export default App;
+const styles = StyleSheet.create({
+  headerContainer: {
+    backgroundColor: 'red',
+    flex: 2,
+  },
+  mainContainer: {
+    backgroundColor: 'grey',
+    flex: 4,
+  },
+  footerContainer: {
+    flex: 1,
+    backgroundColor: 'blue',
+  },
+});
