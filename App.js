40e33aa3f5d1ae27159e5f4713e6b4134f29d0b0
burakseyhann <burakseyhan99@hotmail.com>
2021-01-31 22:10:52 +0300

    icon
    icon

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 const App = () => (
   <View>
-    <Text>Login Screen</Text>
+    <Text>Login Screenn</Text>
+    <Ionicons name={'ios-menu-outline'} color='red' size={35} />
   </View>
 );
 
