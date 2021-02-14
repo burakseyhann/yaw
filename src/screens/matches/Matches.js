d78e98d9d9e8acc29ca0310924a7531382c19ae4
burakseyhann <burakseyhan99@hotmail.com>
2021-02-14 22:13:31 +0300

    new dew
    new dew

--- src/screens/matches/Matches.js
+++ src/screens/matches/Matches.js
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 function Matches  ({navigation}){
     return(
     <View>
-        <Text onPress={()=>{navigation.navigate('ProfileDetails')}}>Matches</Text>
+        <Text onPress={()=>{navigation.navigate('ProfileDetails')}}>Matchessss</Text>
     </View>
 );
 }
